7a87551ceac59631bff59d69706914a19380934a
cyril jardin <cyilroc60@hotmail.fr>
2018-06-28 09:06:48 +0200

    jasminev2

---
+++ exojsjasminev2/questions.js
@@ -0,0 +1,52 @@
+var languagesArrayCreation = function () {
+  var languages = new Array('Html', 'CSS', 'Java', 'PHP');
+  return languages;
+}
+
+var numbersArrayCreation = function () {
+    var numbers = new Array(0, 1, 2, 3, 4, 5);
+    return numbers;
+}
+
+var ElementReplacement = function (languages) {
+  languages[2] = 'Javascript';
+  return languages;
+}
+
+var AddElementToLanguagesArray = function (languages) {
+  languages.push('Ruby','Python');
+  return languages;
+}
+
+var AddElementToNumbersArray = function (numbers) {
+  numbers.unshift(-2,-1);
+  return numbers;
+}
+
+var deleteArrayFirstElement = function (languages) {
+  languages.shift();
+  return languages;
+}
+
+var deleteArrayLastElement = function (languages) {
+  languages.pop();
+  return languages;
+}
+
+var stringToArray = function (socialMediaInString) {
+  var socialMedia = socialMediaInString.split(',');
+  return socialMedia;
+}
+
+var arrayToString = function (languages) {
+  var languagesInString = languages.join(',');
+  return languagesInString;
+}
+
+var arraySort = function (socialMedia) {
+  return socialMedia.sort();
+}
+
+var arrayInvert = function (socialMedia){
+  return socialMedia.reverse();
+}
